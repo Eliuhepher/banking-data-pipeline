f22ec8bde5e1c515d5b9c6cab971eeb3601325ff
Eliu Hepher G. <eliu.hepher@gmail.com>
2026-07-08 02:45:54 -0600

    fix: update OIDC provider output to use data source

--- terraform/iam/outputs.tf
+++ terraform/iam/outputs.tf
@@ -1,4 +1,4 @@
 output "github_infra_role_arn"      { value = aws_iam_role.github_infra.arn }
 output "github_jobs_role_arn"       { value = aws_iam_role.github_jobs.arn }
 output "github_monitoring_role_arn" { value = aws_iam_role.github_monitoring.arn }
-output "oidc_provider_arn"          { value = aws_iam_openid_connect_provider.github.arn }
+output "oidc_provider_arn"          { value = data.aws_iam_openid_connect_provider.github.arn }
